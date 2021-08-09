409a50024733bd25302aea9b3be3a275492786a2
soudabeh19 <s_arghi@encs.concordia.ca>
2021-08-09 00:05:35 -0400

    first draft

---
+++ search_word_documents.py
@@ -0,0 +1,23 @@
+def is_word_there(list_docs, keyword):
+    """
+    search the keyword inside of a list of documents . Each document is a string element of a list.
+    desire output should show in which documents the keyword is mentioned. 
+	in your search don't consider . and , also it should not be case sensitive. in addition the output should not refelect any words that contains the keyword (keyword without pre or pro fix)
+
+    Example:
+    list_docs = ["The is a new document.", "several books are here", "Book in here has a different color"]
+    >>> is_word_there(list_docs, 'casino')
+    >>> [2]
+    """
+    out_list=[]
+    for element in list_docs:
+        lst_words=element.split()
+        normal = [word.rstrip('.,').lower() for word in lst_words]
+        if keyword in normal:
+            out_list.append(list_docs.index(element))
+           
+    return(out_list)
+        
+
+list_docs = ["The is a new document.", "several books are here", "Book in here has a different color"]
+print(is_word_there(list_docs, 'book'))
